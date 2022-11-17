777121cd45db8cae90a68e1e554e90b2d9ec6ec0
Alberto Rivera <alrie2000@hotmail.com>
2022-11-16 21:53:16 -0600

    Esta es la última versión de la tarea. 	Para terminar se implementó cross-entropy al codigo. la 	eficiencia de la red se mantuvo en 96 %, sin embargo, podemos 	decir que mejoró su rendimiento ya que antes de hacer este 	cambio, el aprendizaje de la red era lenta (iniciando en 	alrededor del 70 o 60 %), ahora vemos que desde la primera epoca 	consseuimos un porcentaje del 90 %.

--- network.py
+++ network.py
@@ -114,8 +114,7 @@ class Network(object):
             activation = sigmoid(z)
             activations.append(activation)
         # backward pass
-        delta = self.cost_derivative(activations[-1], y) * \
-            sigmoid_prime(zs[-1])
+        delta = self.cost_derivative(activations[-1], y)
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
         # Note that the variable l in the loop below is used a little
